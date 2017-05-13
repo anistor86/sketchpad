163f82df60b5ff7c78e0f270ebc70c740dbac117
Luca Storani <lucastorani@gmail.com>
2017-05-13 12:17:32 +0100

    Added cicle that print boxes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-var numSquare = 8;
+$(document).ready(function(){
 
-for(var i = 1; i <= numSquare; i++){
+  var numBox = 10;
+  var box = '<div class="box"></div>';
+  for(var i = 1; i <= numBox; i++){
+    for(var j = 1; j <= numBox; j++){
+      $('#container').append('<div class="box"></div>');
+    }
+  }
 
-}
+});
