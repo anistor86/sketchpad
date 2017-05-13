49aaf34f65a1c19193f2bcdc895201b151a57a8e
Luca Storani <lucastorani@gmail.com>
2017-05-13 15:52:52 +0100

    Fix create grid function + boxes resize

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 
-  var numBox = 10;
-  var box = '<div class="box"></div>';
-  for(var i = 1; i <= numBox; i++){
-    for(var j = 1; j <= numBox; j++){
-      $('#container').append('<div class="box"></div>');
+  //Create the grid and resize the boxes
+  function createGrid(numBox){
+    var box = '<div class="box"></div>';
+    for(var i = 1; i <= numBox; i++){
+      for(var j = 1; j <= numBox; j++){
+        $('#container').append('<div class="box"></div>');
+      }
     }
+    $('.box').css({'height': checkSize(numBox), 'width': checkSize(numBox)});
   }
 
+  //check the number of boxes and resize them 
+  function checkSize(numBox){
+    var size = (400 - (numBox * 2)) / numBox;
+    return size;
+  }
+
+createGrid(2);
 });
